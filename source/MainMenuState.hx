9ae70dfb62bd66dd254c550cc3e9037613b5171a
RenanGamerProOficial <69207411+RenanGamerProOficial@users.noreply.github.com>
2022-04-24 11:06:03 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -131,7 +131,7 @@ class MainMenuState extends MusicBeatState
 		FlxG.camera.follow(camFollowPos, null, 1);
 
                 char1 = new Character(800, -130, 'gf', true);
-                char1.setGraphicSize(Std.int(char1.width * 0.8));\
+                char1.setGraphicSize(Std.int(char1.width * 0.8));
                 add(char1);
                 char1.visible = false;
 
