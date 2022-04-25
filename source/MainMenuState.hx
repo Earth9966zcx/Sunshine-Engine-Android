3fd9f113ab84681388a5dd5f24f1ab8ba4eabfca
RenanGamerProOficial <69207411+RenanGamerProOficial@users.noreply.github.com>
2022-04-25 10:12:48 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var sunshineEngineVersion:String = '0.1.3';
+	public static var sunshineEngineVersion:String = '0.1.4';
 	public static var psychEngineVersion:String = '0.5.2h'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
